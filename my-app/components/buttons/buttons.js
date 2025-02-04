a912131ec9c5d4cb7e15aebda2471811274376e5
ZixHD <havos25@gmail.com>
2025-02-04 01:04:23 +0100

    starting buttons

---
+++ my-app/components/buttons/buttons.js
@@ -0,0 +1,19 @@
+
+import './buttons.css'
+
+export default function MyButton(){
+    return(
+        <div className='main-container'>
+            
+            <div className="button-container">
+                <div className="my_button">
+                    <span className="text">Yes</span>
+                </div>
+                <div className="my_button">
+                    <span className="text">No</span>
+                </div>
+            </div>
+        
+        </div>
+    )
+}
